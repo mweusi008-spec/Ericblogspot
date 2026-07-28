a11e4d00a00d6ac64d90711e3e3ce78569ab1310
mweusi008-spec <mweusi008@gmail.com>
2026-07-29 00:12:53 +0300

    r

--- core/views.py
+++ core/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "home.html")
+
+    breaking_news = [
+        {
+            "title": "OpenAI releases a new AI model",
+            "category": "AI",
+        },
+        {
+            "title": "Gold price climbs as USD weakens",
+            "category": "Forex",
+        },
+        {
+            "title": "Critical Windows security vulnerability discovered",
+            "category": "Cybersecurity",
+        },
+    ]
+
+    return render(request, "home.html", {
+        "breaking_news": breaking_news
+    })
