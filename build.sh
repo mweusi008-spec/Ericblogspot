284f81ca74892ba7d568d3eea9f1aeed995bebad
mweusi008-spec <mweusi008@gmail.com>
2026-07-29 19:52:04 +0300

    Fix deployment config: env vars, build script, remove jazzmin references

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Render build script for Django
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
